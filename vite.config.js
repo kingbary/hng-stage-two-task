0513743d4f0d71d8efe7989186da71377af6e64e
Kingsley <kingsleyakwa@gmail.com>
2023-09-16 22:27:10 +0100

    added full url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hng-stage-two-task/'
+  base: "https://kingbary.github.io/hng-stage-two-task"
 })
